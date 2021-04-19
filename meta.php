64ac788d90a8954c0b51cfd5da03eafa99afdd48
JamesTheN00B <hiranmaydas1999@gmail.com>
2021-04-19 17:17:10 +0530

    Added Montserrat

--- meta.php
+++ meta.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles/styles.css?<?php echo time(); ?>">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
     <script src="script.js"></script>
     <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css"> -->
     <!-- BOOTSTRAP IMPORT LINK -->
